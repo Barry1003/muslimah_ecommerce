08013a8af2f6fdc3834dd690ca1b85d4a81a1d5c
symplymuslimah <104183464+symplymuslimah@users.noreply.github.com>
2023-08-22 22:07:35 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,10 @@
+
+ //nav script code
+const iconNav = document.querySelector(".icon-nav");
+iconNav.addEventListener('click',function() {
+  const  navBar = document.querySelector('.nav-1');
+    navBar.classList.toggle("active");
+    
+});
+
+
